e2ce910bc52f0a62f31ccddd27eaa68fce82dd19
May7331 <purpleseed1107@gmail.com>
2023-09-14 13:42:21 +0800

    Responsive done

--- src/components/PairItem.vue
+++ src/components/PairItem.vue
@@ -26,7 +26,7 @@ watch([value], ([value]) => {
 <template lang="pug">
 div(:key="pair.id" class="px-5 py-2 flex")
   div(class="grid grid-cols-2 gap-4 flex-grow mr-3")
-    input(type="text" v-model="key" :placeholder="pair.key" class="bg-transparent outline-none border text-slate-200 border-slate-500 text-base px-2 py-1 w-full placeholder-slate-200")
+    input(type="text" v-model="key" :placeholder="pair.key" class="bg-transparent outline-none border  border-slate-500 text-base px-2 py-1 w-full placeholder-slate-200")
     input(type="text" v-model="value" :placeholder="pair.value" class="bg-transparent outline-none border text-slate-200 border-slate-500 text-base px-2 py-1 w-full placeholder-slate-200")
   button(@click.prevent="deletePair(pair.id)" class="w-10 text-sm flex justify-center items-center bg-slate-300 text-slate-800") -
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,11 @@ import TreeViewer from "../components/TreeViewer.vue";
 </script>
 
 <template lang="pug">
-div(class="px-20 pb-20 pt-5 h-screen bg-slate-900")
-  h2(class="mb-2 text-2xl text-slate-200 font-bold") Nested Key-Value Pair Tree Viewer
-  div(class="grid grid-cols-2 gap-8 h-full")
+div(class="px-10 pb-20 pt-5 h-screen bg-slate-900 text-slate-200 overflow-y-scroll sm:px-20 md:px-10")
+  h2(class="mb-2 text-2xl font-bold") Nested Key-Value Pair Tree Viewer
+  div(class="grid grid-cols-1 gap-8 h-full md:grid-cols-2 ")
     div(class="border border-slate-500")
       PairList
-    div(class="text-slate-200 p-6 border border-slate-500")
+    div(class=" border border-slate-500  p-6")
       TreeViewer
 </template>
